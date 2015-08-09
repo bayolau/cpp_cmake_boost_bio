d0560425a983dda4329514397f6b0433e2216d3e
Bayo Lau <bayo.lau@gmail.com>
2015-08-09 15:16:39 -0700

    added seqan to build system

--- src/bare.cpp
+++ src/bare.cpp
@@ -1,17 +1,24 @@
-
 #include <iostream>
 
 #include "CMakeConfig.h"
 
+#ifdef USE_SEQAN
+#include <seqan/basic.h>
+#include <seqan/sequence.h>
+#include <seqan/stream.h>
+#endif
+
 #ifdef USE_FOO
 #include "foo.h"
 #endif
-
 
 int main(int argc, char* argv[]) {
   std::cout << VERSION_MAJOR << std::endl;
   std::cout << VERSION_MINOR << std::endl;
 #ifdef USE_FOO
   junk::foo::printTo(std::cout);
+#endif
+#ifdef USE_SEQAN
+  std::cout << seqan::CharString("Using SeqAn!") << std::endl;
 #endif
 }
